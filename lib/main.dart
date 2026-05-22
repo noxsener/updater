d5f66133bda1e2a68579eda3b191ce4e1fcb82c0
noxsener <noxsener@gmail.com>
2026-05-22 14:48:20 +0300

    Düzenlemeler

--- lib/main.dart
+++ lib/main.dart
@@ -539,7 +539,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                   ),
                 if (_fileProgress <= 0 || _fileProgress >= 1.0)
                   _FileProgressBar(
-                    progress: 1,
+                    progress: 0,
                   ),
                 const SizedBox(height: 24),
 
