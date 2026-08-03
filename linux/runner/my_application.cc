3f9f044daf17d72cb23ec6e67bd18cb97b9cdb2a
noxsener <noxsener@gmail.com>
2026-08-03 23:55:46 +0300

    Düzenlemeler

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -54,6 +54,19 @@ static void my_application_activate(GApplication* application) {
 
   gtk_window_set_default_size(window, 1280, 720);
 
+  // Set the application/window icon. The icon file is installed next to the
+  // executable by the CMake install rules (see linux/CMakeLists.txt).
+  g_autofree gchar* exe_path = g_file_read_link("/proc/self/exe", nullptr);
+  if (exe_path != nullptr) {
+    g_autofree gchar* exe_dir = g_path_get_dirname(exe_path);
+    g_autofree gchar* icon_path =
+        g_build_filename(exe_dir, "app_icon.png", nullptr);
+    g_autoptr(GError) icon_error = nullptr;
+    if (!gtk_window_set_icon_from_file(window, icon_path, &icon_error)) {
+      g_warning("Uygulama ikonu yuklenemedi: %s", icon_error->message);
+    }
+  }
+
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(
       project, self->dart_entrypoint_arguments);
